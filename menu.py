9ade96cd28d2e9b0da96ae9b3af38c6272329681
DuckyDav <150874711+Duckydav@users.noreply.github.com>
2025-02-26 11:49:33 -0500

    Update menu.py

--- menu.py
+++ menu.py
@@ -5,3 +5,4 @@ import gui
 dd_tools_menu = nuke.menu('Nuke').addMenu('DD Tools')
 dd_tools_menu.addCommand('$gui', 'gui.run()', 'Alt+D')
 dd_tools_menu.addCommand('$gui all', 'gui.run_all()', 'Alt+4')
+dd_tools_menu.addCommand('$gui setting', 'gui.setting()', 'Shift+Alt+D')
